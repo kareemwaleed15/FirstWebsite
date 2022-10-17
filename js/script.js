909e56c3a7dba01acd49ac060fd203b9581cdad2
kareemwaleed15 <110359582+kareemwaleed15@users.noreply.github.com>
2022-10-17 16:40:10 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(".owl-carousel").owlCarousel({
+  stagePadding: 0 ,
+  loop: true,
+  rtl: false,
+  lazyLoad: true,
+  margin: 0,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    800: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+    1200: {
+      items: 3,
+    },
+  },
+});
